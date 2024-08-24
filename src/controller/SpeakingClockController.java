51a123496b4e16fe1a0a8ff0f8513228264aa7c9
Dineshkalimisetty <Dineshkalimisetty@Dinesh>
2024-08-24 11:51:53 +0530

    first commit

---
+++ src/controller/SpeakingClockController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.util.Scanner;
+
+import model.TimeResponse;
+import service.TimeConversionService;
+
+public class SpeakingClockController {
+
+    private TimeConversionService timeConversionService;
+
+    public SpeakingClockController() {
+        this.timeConversionService = new TimeConversionService();
+    }
+
+    public TimeResponse getCurrentTime() {
+        String currentTime = java.time.LocalTime.now().toString().substring(0, 5);
+        String timeInWords = timeConversionService.convertTimeToWords(currentTime);
+        return new TimeResponse(timeInWords);
+    }
+
+    public TimeResponse convertTime(String time) {
+        String timeInWords = timeConversionService.convertTimeToWords(time);
+        return new TimeResponse(timeInWords);
+    }
+
+    public static void main(String[] args) {
+        SpeakingClockController controller = new SpeakingClockController();
+
+        // Example usage
+        System.out.println("Enter a time in HH:mm format or type 'current' for the current time:");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        TimeResponse response;
+        if (input.equalsIgnoreCase("current")) {
+            response = controller.getCurrentTime();
+        } else {
+            response = controller.convertTime(input);
+        }
+
+        System.out.println(response.getTimeInWords());
+    }
+}
---
+++ src/model/TimeResponse.java
@@ -0,0 +1,17 @@
+package model;
+
+public class TimeResponse {
+    private String timeInWords;
+
+    public TimeResponse(String timeInWords) {
+        this.timeInWords = timeInWords;
+    }
+
+    public String getTimeInWords() {
+        return timeInWords;
+    }
+
+    public void setTimeInWords(String timeInWords) {
+        this.timeInWords = timeInWords;
+    }
+}
